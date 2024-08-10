70c3c88b2a7bffd0580cec28872a00476ef79605
gabriel <gabovchoque@gmail.com>
2024-08-09 21:22:54 -0400

    S4 Proyecto Base VUE

---
+++ src/views/LoginView.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+         {{ usuario }}
+        <h1>Login</h1>
+        <label for="e">Ingresar Correo:</label>
+        <input type="email" v-model="usuario.email">
+        <!--<span>{{ errors.email }}</span>-->
+        <br>
+        <label>Ingresar Contraseña:</label>
+        <input type="password" v-model="usuario.password">
+        <!--<span>{{ errors.password }}</span>-->
+        <br>
+        <input type="button" value="Ingresar" v-on:click="funIngresar">
+        
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import axios from 'axios';
+
+    //variables
+    const usuario = ref({email:"", password:""})
+
+    //metodos
+    async function funIngresar() {
+        axios.post("http://127.0.0.1:8000/api/v1/auth/login", usuario.value).then(
+            (res) => {
+                console.log("CON AXIOS", res.data)
+            }
+        )
+    }
+</script>
